276fe53e7d2dfc133def9df50d91f7e6232ad5c6
zxc9802 <785755358@qq.com>
2026-08-26 17:26:09 +0800

    fix: explicit baseUrl in tsconfig and webpack alias in next.config for Linux Docker build

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '300mb',
+      bodySizeLimit: "300mb",
     },
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
